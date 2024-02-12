df24a44c743725d585f6217dc0219460903e6cd8
Hitesh Patiyal <hiteshpatiyal2566@gmail.com>
2024-02-12 23:04:35 +0530

    add new POM men_top_jacket_page

---
+++ src/test/java/pageObjects/Mens_Top_Jacket_Page.java
@@ -0,0 +1,10 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class Mens_Top_Jacket_Page extends BasePage{
+	
+	public Mens_Top_Jacket_Page(WebDriver driver) {
+		super(driver);
+	}
+}
