018eb95bfe252ecc6bd2ca1ff4a7f31dd56cfe87
Hitesh Patiyal <hiteshpatiyal2566@gmail.com>
2024-02-12 23:15:09 +0530

    made changes in womens top jacket page file

--- src/test/java/pageObjects/Womens_Top_Jackets_Page.java
+++ src/test/java/pageObjects/Womens_Top_Jackets_Page.java
@@ -1,7 +1,8 @@
 package pageObjects;
 import org.openqa.selenium.WebDriver;
-public class Womens_Top_Jackets_Page{
- public void Womens_Top_Jackets_Page(WebDriver driver){
-  super(driver)
+
+public class Womens_Top_Jackets_Page extends BasePage{
+ public Womens_Top_Jackets_Page(WebDriver driver){
+	 super(driver);
 }
 }
